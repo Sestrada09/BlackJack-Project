e0486f9b0a0d898427e8c3f9cd4e59bf0c48eefc
Diane Estrada <Samantha@Dianes-MacBook-Pro-2.local>
2022-03-22 18:40:28 -0500

    added card class and GitCommandHelp

---
+++ src/Card.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+public class Card {
+    private int cardValue;
+    public Card() {
+        cardValue = (int)(Math.random()*(1-12+1)+1);
+    }
+    public int getCardValue() {
+        return cardValue;
+    }
+}
